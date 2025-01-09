94412ce3f416532227276244c7b1a3311617aefd
Arputha Joshwa B <146755994+Joshwa37@users.noreply.github.com>
2025-01-09 17:26:42 +0530

    Add files via upload

---
+++ PatternDiamond.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+public class Area {
+    public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+        int num=s.nextInt();
+    int f1=(num+1)/2;
+    int f2=(num+1)/2;
+    int i=1;
+    while(i<=num){
+    for(;i<=num;i++){
+        for(int j=1;j<=num;j++){
+            if(i<=(num+1)/2&&(j<=f1&&j>=f2)||i>(num+1)/2&&(j<=f1&&j>=f2)){
+                System.out.print("* ");
+            }
+            else{
+                System.out.print("  ");
+            }
+
+        }
+        if(i<(num+1)/2){
+         ++f1;
+         --f2;
+        }
+        else{
+            --f1;
+            ++f2;
+        }
+         System.out.println();
+
+         }
+        }
+    }
+    
+}
